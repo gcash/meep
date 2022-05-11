dc12581043e13743ff6ef93627957da722d74ccc
quest <quest@mac.com>
2022-05-11 03:08:12 -1000

    Bump version to 0.4.0

--- version.go
+++ version.go
@@ -13,8 +13,8 @@ const semanticAlphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqr
 // versioning 2.0.0 spec (http://semver.org/).
 const (
 	AppMajor uint = 0
-	AppMinor uint = 3
-	AppPatch uint = 1
+	AppMinor uint = 4
+	AppPatch uint = 0
 
 	// AppPreRelease MUST only contain characters from semanticAlphabet
 	// per the semantic versioning spec.
